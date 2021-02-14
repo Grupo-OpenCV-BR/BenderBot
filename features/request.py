8edfc1697253c3f6f0681e66a6d7268836b160a7
Andre Emidio <andresjc2008@gmail.com>
2021-02-13 23:08:38 -0300

    Aumento do tempo da DontStopMeNow

--- features/request.py
+++ features/request.py
@@ -4,6 +4,6 @@ import requests
 
 
 def DontStopmeNOW():
-    threading.Timer(600.0, DontStopmeNOW).start()
+    threading.Timer(600000, DontStopmeNOW).start()
     requests.get('http://www.google.com')
     print("I did a request!")

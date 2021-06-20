843a4071f12bb65c7062d59a5f56405bdb835415
Andre Emidio <andresjc2008@gmail.com>
2021-06-20 13:38:37 -0300

    Debug off

--- core.py
+++ core.py
@@ -52,8 +52,8 @@ def main():
     dispatcher.add_handler(sys_handler)
     dispatcher.add_error_handler(error)
 
-    if DEBUG is True:
-        updater.start_polling()
+    # if DEBUG is True:
+    #     updater.start_polling()
 
     logging.info(f'Porta de comunicação {PORT}')
 

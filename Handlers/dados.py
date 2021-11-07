d5e46359b5cf82c2fa21a728c5c1b809594a567c
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2021-11-07 00:30:52 -0300

    adicionado: salvar aquivos

---
+++ Handlers/dados.py
@@ -0,0 +1,15 @@
+import json
+
+
+def salva(mensagem, table, colum):
+    with open('./dados.json', 'r') as dados:
+        eventos = json.load(dados)
+        if table in eventos:
+            eventos[table].append({colum: mensagem})
+        else:
+            eventos[table] = []
+            eventos[table].append({colum: mensagem})
+
+    with open('./dados.json', 'w') as dados:
+        json.dump(eventos, dados, indent=4)
+
